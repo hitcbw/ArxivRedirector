f45ae5164c242875dde97a6844fb3c80ed902f28
hitcbw <hitcbw@outlook.com>
2021-01-13 09:50:05 +0800

    first code commit

---
+++ js/background.js
@@ -0,0 +1,29 @@
+chrome.contextMenus.create({
+	title: "Redirect!",
+	onclick: main,
+	contexts: ["all"]
+});
+
+function main() {
+	chrome.commands.onCommand.addListener(
+		chrome.tabs.query({
+			currentWindow: true,
+			active: true
+		}, function (tabs) {
+			if (tabs[0] && tabs[0].url) {
+				var url = tabs[0].url;
+				// arxiv.org/abs AND arxiv.org/pdf pattern
+				var pattern_arxiv = /((http|https):\/\/arxiv.org\/.+)/;
+				// match arxiv.org/abs
+				if (pattern_arxiv.test(url)) {
+					// replace links
+					var pattern_paper_id = /((https|http):\/\/arxiv.org\/)/;
+					var new_url = url.replace(pattern_paper_id, "http://xxx.itp.ac.cn/");
+					chrome.tabs.update({
+						url: new_url
+					});
+				}
+			}
+		})
+	)
+}
